a47da0fdd4a36321a7293bd90c7a00e986ba4ce7
PhanuphongKuharungkitcharoen <Phanuphongkuharungkitcharoen@gmail.com>
2025-11-28 14:45:09 +0700

    first commit

---
+++ day01/practices/01.ts
@@ -0,0 +1,23 @@
+//console.log
+
+// ' ' , " " 
+console.log("Hello world");
+console.log('Hello world');
+console.log("'phanuphong' is my name");
+console.log('"phanuphong" is my name');
+
+//int float 
+console.log(123);
+console.log(12.3);
+
+//Operation 
+console.log(3+3);
+console.log(3-3);
+console.log(3*3);
+console.log(3/3);
+console.log(3%3);
+console.log(3^22); //bitwiseXOR
+
+console.log("Age:",20);
+console.log("Name:","phanuphong","Surname:","Kuharungkitcharoen")
+
---
+++ day01/practices/03.ts
@@ -0,0 +1,9 @@
+export = {}; //"Treat this file as its own separate thing (a Module)."
+
+let name  = "Austin";
+let age = 19 ;
+
+console.log(`Name: ${name}`);
+console.log("Age:", age);
+console.log("Math:",5+10);
+console.log("Name :",name);
